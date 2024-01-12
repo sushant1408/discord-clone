c77908934a0fe7d0918e58559372af9fa9df0aef
Sushant Gandhi <gandhi.sushant1408@gmail.com>
2024-01-12 13:30:06 +0530

    added theme switcher

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ThemeToggle />
     </div>
   );
 };
