c7dce183f4693f04469c87d5377099f6b80f245a
Sushant Gandhi <gandhi.sushant1408@gmail.com>
2024-01-12 13:14:13 +0530

    clerk integration, sign-in sign-up routes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 const Home = () => {
   return (
     <div>
-      This is a protected route.
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
